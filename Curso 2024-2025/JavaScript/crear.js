42224e8f3bb1d33daa571e7b2a63595edb60c46b
Juan Carlos Cuevas Martínez <jccuevas@users.noreply.github.com>
2025-02-18 20:38:59 +0100

    Comentarios y ajustes tras la clase

--- Curso 2024-2025/JavaScript/crear.js
+++ Curso 2024-2025/JavaScript/crear.js
@@ -1,40 +1,47 @@
-let boton = document.getElementById("botonCrear")
-boton.addEventListener("click", crearParrafo)
-const datos = '[{ "_id": "65a8ead6251befc5cb0d3bc2", "user": "usuario", "title": "Entrada 1", "date": "2024-03-01", "comment": "Esta es una entrada del blog" }, { "_id": "65a8eaea251befc5cb0d3bc3", "user": "usuario", "title": "Entrada 2", "date": "2024-01 - 18", "comment": "Esta es otra entrada del blog" }, { "_id": "66163c2ae5230f36fbcee0b4", "user": "usuario", "title": "Entrada 4", "date": "2024-04 - 10", "comment": "Son ya tres las entradas en el blog" }, { "_id": "66163c3ce5230f36fbcee0b5", "user": "usuario", "title": "Entrada 4", "date": "2024-04 - 10", "comment": "Son ya cuatro las entradas en el blog" }, { "_id": "66163c50e5230f36fbcee0b6", "user": "usuario", "title": "Entrada 5", "date": "2024-04 - 10", "comment": "Son ya cinco las entradas en el blog" }]'
+/* Este script se emplea en dom.html */
 
-function crearParrafo() {
-    let ul = document.getElementById("texto");
-    ul.innerHTML = "";
-    try {
-        const entradas = JSON.parse(datos)
-        for (let entrada of entradas) {
-            ul.appendChild(crearLi(entrada))
-        }
+/* Le añadimos un evento a un botón a través del DOM */
+let boton = document.getElementById("botonCrear");
+boton.addEventListener("click", crearElemento); //Añadimos un evento click al botón que llama la función crearElemento
+
+//Datos de ejemplo cargados de un servicio web
+const datos =
+  '[{ "_id": "65a8ead6251befc5cb0d3bc2", "user": "usuario", "title": "Entrada 1", "date": "2024-03-01", "comment": "Esta es una entrada del blog" }, { "_id": "65a8eaea251befc5cb0d3bc3", "user": "usuario", "title": "Entrada 2", "date": "2024-01 - 18", "comment": "Esta es otra entrada del blog" }, { "_id": "66163c2ae5230f36fbcee0b4", "user": "usuario", "title": "Entrada 4", "date": "2024-04 - 10", "comment": "Son ya tres las entradas en el blog" }, { "_id": "66163c3ce5230f36fbcee0b5", "user": "usuario", "title": "Entrada 4", "date": "2024-04 - 10", "comment": "Son ya cuatro las entradas en el blog" }, { "_id": "66163c50e5230f36fbcee0b6", "user": "usuario", "title": "Entrada 5", "date": "2024-04 - 10", "comment": "Son ya cinco las entradas en el blog" }]';
+
+function crearElemento() {
+  let ul = document.getElementById("texto");
+  ul.innerHTML = "";
+  try {
+    const entradas = JSON.parse(datos);
+    for (let entrada of entradas) {
+      ul.appendChild(creaLi(entrada));
     }
-    catch (error) {
-        console.error("Error: " + error)
-    }
+  } catch (error) {
+    console.error("Error: " + error);
+  }
 }
 
+/* Función que crea un elemento de la lista
+Debemos evitar crear funciones que realicen varias operaciones,
+por eso sacamos esta función fuera de crearElemento */
 function creaLi(entrada) {
-    let li = document.createElement("li")
-    let title = document.createElement("strong")
-    let date = document.createElement("i")
-    let comment = document.createElement("p")
-    let button = document.createElement("button")
-    li.setAttribute("id", entrada._id)
-    title.innerText = entrada.title
-    date.innerText = " [" + entrada.date + "]"
-    comment.innerText = entrada.comment
-    button.innerText = "Enviar"
-    button.addEventListener("click", () => borraElemento(entrada._id))
+  let li = document.createElement("li");
+  let title = document.createElement("strong");
+  let date = document.createElement("i");
+  let comment = document.createElement("p");
+  let button = document.createElement("button");
+  li.setAttribute("id", entrada._id);
+  title.innerText = entrada.title;
+  date.innerText = " [" + entrada.date + "]";
+  comment.innerText = entrada.comment;
+  button.innerText = "Enviar";
+  button.addEventListener("click", () => borraElemento(entrada._id));
 
-    li.append(title, date, comment, button)
-    return li
+  li.append(title, date, comment, button);
+  return li;
 }
 
 function borraElemento(id) {
-    let borrar = document.getElementById(id);
-    borrar.remove();
-
+  let borrar = document.getElementById(id);
+  borrar.remove();
 }
